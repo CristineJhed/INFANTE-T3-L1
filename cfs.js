980c3c7bf2e90638e53bd696c6f29ad1ddb4bccc
CristineJhed <145459239+CristineJhed@users.noreply.github.com>
2024-05-05 20:19:28 +0800

    Add files via upload

---
+++ cfs.js
@@ -0,0 +1,41 @@
+let name ='Cristine Jhed Infnte';
+let address= 'Paypayad';
+let age =20;
+let ClassRole= 'Student';
+
+const course = "BSCS";
+
+//CONDITIONAL STATEMENT
+if (course === "BSCS"){
+    
+    if(ClassRole === "Office");{
+
+        console.log("Officer Detected!");
+
+    } if (ClassRole == "Student"){
+    var password = "BSCS1234";
+    if (password >= 12){
+
+        console.log("Student Name:" + Name);
+
+        for (var i = 0; i < age; i++){
+        console.log ("Address:" +address);
+    }
+} else {
+
+    console.log("invalid password");
+}
+} else if (classRole == "Teacher"){
+
+    console.log("Teacher Detected");
+} else {
+
+    console.log("invalid class role");
+    }
+}else if (course== "BSM"){
+
+}else if (course == "BAEL"){
+    
+}else {
+    console.log("invalid course");
+}
